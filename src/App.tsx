9446a2c2a4abb33f693c8e9ed2d963c2aa9b7102
Flávio Nassim Bittar Filho <flavionassimbittarfilho@gmail.com>
2023-03-11 09:40:24 -0300

    modified functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-export const App = () => {
+export function App() {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center bg-[url("src/assets/img/world.jpg")] bg-cover bg-center'>
       <div className='text-8xl text-blue-200'>Hello World</div>
